45140de94b46bd3c5c55f6b2da81cb52d5a04077
Thai Vin <thaivinnguyen2015@gmail.com>
2018-04-22 23:00:33 +0700

    create file in project

--- code/decision.py
+++ code/decision.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-#comment
 
 # This is where you can build a decision tree for determining throttle, brake and steer 
 # commands based on the output of the perception_step() function
